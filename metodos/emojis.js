efc6e594dfaf55647d3d0f1a25cdae33c6214c73
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-02-25 15:33:55 -0300

    aulas

---
+++ metodos/emojis.js
@@ -0,0 +1,31 @@
+//9SOME()
+// [🏀, 🏓, 🍎, 🍕, 🥝, 🐳, 🍎].some(🍕) -> true
+//3 existe piza? me retorna true
+
+//9EVERY()
+// [🏀, 🏓, 🍎, 🍕, 🥝, 🐳, 🍎].every(🍎) -> false;
+//3 todos os elementos do array são maçã? me retorna false
+
+//9FIND()
+// [🏀, 🏓, 🍎, 🍕, 🥝, 🐳, 🍎].find(🥝) -> 🥝;
+//3 existe um quiui? se existir?  me retorna um OBS, TRAS APENAS O PRIMEIRO QUE ENCONTRAR
+
+//9FILTER()
+// [🏀, 🍕, 🍎, 🍕, 🍕, 🐳, 🍎].filter(🍕) -> [🍕, 🍕, 🍕];
+//3 tem pedaço de pixa? se tiver traz todos dento de um array 
+
+//9FOREACH()
+// [2, 4, 6, 8, 10].forEach(* 2) -> [4, 8, 12, 16, 20]
+//3 tem a função de altera cada elemento de um array. OBS sai um array
+
+//9SORT()
+// [7, 5, 8, 3, 2, 1, 4, 6].sort() -> [1, 2, 3, 4, 5, 6, 7, 8]
+//3  e uma função que ordena os numeros ou alfabeto em ordem crescente. Obs sai um array
+
+//9MAP()
+// [🐮, 🥔, 🌽, 🪵, 🍎].map(🏭) -> [🍔, 🍟, 🍿, 🪑, 🥧]
+//3 transfomar os elementos de um array. cria um novo array com os mesmos elementos
+
+//9REDUCE()
+// [🍔, 🍟, 🍿, 🥧, 🍩].reduce(🍽) -> 💩
+//3 pega todos os elemento e reduz a um elemento só
