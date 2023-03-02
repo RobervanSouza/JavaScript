b86143474a656c3b2d6306017deebc7939bdf50b
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-03-02 09:03:36 -0300

    anos

---
+++ importante/exercicio.js
@@ -0,0 +1,12 @@
+// mostra o seculo 
+
+function solution(year) {
+   return Math.ceil(year/100)
+
+}
+const ano = 140
+
+const ss = ano /100
+console.log(ss)
+const tes = Math.ceil(ss)
+console.log(tes)
