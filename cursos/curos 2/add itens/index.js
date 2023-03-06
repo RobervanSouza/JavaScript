51bfd76235a765a56a7140062cfb2e4262f76e94
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-03-06 13:30:36 -0300

    aulas

--- cursos/curos 2/add itens/index.js
+++ cursos/curos 2/add itens/index.js
@@ -8,33 +8,36 @@ test[ 9 ] = "Tudo bem?"
 test[ 10 ] = "Opa!"
 let meses = [ 'Jan', 'Fev', 'Mar', 'Abr' ]
 meses[ 0 ] = 'Janeiro'
+document.write(produtos)// mostra no html
 
 // ADICIONAR no final push = empurre
-produtos.push('Açúcar', 'Trigo')
-codigos.push(40, 50, 60, 70)
-meses.push('Mai', 'Jun', 'Ago', '07')
+//produtos.push('Açúcar', 'Trigo')
+//codigos.push(40, 50, 60, 70)
+//meses.push('Mai', 'Jun', 'Ago', '07')
+
+
 
 // REMOVER do final pop = estourar
-produtos.pop()
-codigos.pop()
-meses.pop()
-meses.pop()
+//produtos.pop()
+//codigos.pop()
+//meses.pop()
+//meses.pop()
 
 // ADICIONAR no inicio unshift
-produtos.unshift('Uva', 'Maçã')
+//produtos.unshift('Uva', 'Maçã')
 
 // REMOVER do inicio shift
-produtos.shift()
+//produtos.shift()
 
 // REMOVER de uma posicao especifica splice
 // splice = emendar
 // posicao inicial, quantos remover
-codigos.splice(1, 3)
+//codigos.splice(1, 3)
 
 // COPIAR array slice = fatiar porção
 // posicao inicial, quantos copiar
-let meses1 = meses.slice()
-let meses2 = meses.slice(0, 3)
+//let meses1 = meses.slice()
+//let meses2 = meses.slice(0, 3)
 
 // VER TAMANHO DO ARRAY length comprimento
 // meses.length
@@ -42,8 +45,8 @@ let meses2 = meses.slice(0, 3)
 // meses2.lenght
 
 // spreed operator ... copiar
-let teste = [ ...produtos, 'Ovo', 'Pera' ]
+//let teste = [ ...produtos, 'Ovo', 'Pera' ]
 
 // ver indice de um valor no array
 // let pos = nome_array.indexOf('Valor')
-let pos = produtos.indexOf('Uva')
+//let pos = produtos.indexOf('Uva')
