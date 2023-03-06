69f39c30eb6617a9f805e037ff17f5853bc3866b
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-03-06 11:39:10 -0300

    text

--- cursos/aula4.js
+++ cursos/aula4.js
@@ -9,3 +9,7 @@ document.getElementById("get").addEventListener("click", function (){
     var value = document.getElementById("title1").getAttribute("class")
     document.getElementById("class").innerHTML = value
 })
+var titulo = document.querySelector("h2")
+var titulo2 = document.createTextNode("adiciona novo testo")
+titulo.appendChild(titulo2)
+titulo.textContent= "tambem adiciona"
