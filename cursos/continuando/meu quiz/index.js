485761d9cc6fc148f4c3c5fb143a5f3b2e95f9e3
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-03-17 09:26:44 -0300

    finishi

--- cursos/continuando/meu quiz/index.js
+++ cursos/continuando/meu quiz/index.js
@@ -1,25 +1,26 @@
-let instrucoes = document.querySelector('#instrucoes');
+const instrucoes = document.querySelector('#instrucoes');
 let aviso = document.querySelector('#aviso');
 let progresso = document.querySelector('#progresso');
 let pontos = 0;
 let placar = 0;
-
+let acertou = document.querySelector('#acertou');
 let nQuestao = document.querySelector('#numQuestao')
 let pergunta = document.querySelector('#pergunta')
-
+let conteudo = document.querySelector('.conteudo')
 let a = document.querySelector('#a')
 let b = document.querySelector('#b')
 let c = document.querySelector('#c')
 let d = document.querySelector('#d')
-
+let situacao = document.querySelector("#situacao")
 let articleQuestoes = document.querySelector('.questoes')
 // ol li com as alternativas
 let alternativas = document.querySelector('#alternativas')
-
+let media = document.querySelector("#media")
 let numero = document.querySelector('#numero')
 let total = document.querySelector('#total')
-
-
+let placa = document.querySelector(".placa")
+let resultado = document.querySelector(".resultado")
+let botao = document.querySelector("#reiniciar")
 const url = "./data.json"
 function pegarDados(i) {
 
@@ -61,7 +62,7 @@ function atribuirDados(data, index) {
 
     let certa = document.querySelector('#correct')
     certa.value = data.questoes[ index ].correta
-    //console.log(resposta)
+   
 }
 
 // COMECAR O QUIZ
@@ -80,22 +81,25 @@ function verificarSeAcertou(nQuestao, resposta) {
     //console.log("Questão " + numeroDaQuestao)
 
     let respostaEscolhida = resposta.textContent
-    //console.log("RespU " + respostaEscolhida)
+    console.log("RespU " + respostaEscolhida)
 
     // usar o fetch para pegar os dados
     pegarDados(numeroDaQuestao)
 
     let respostaCorrect = correct.value
-    //console.log(respostaCorrect)
+    console.log(respostaCorrect, "resposta")
 
     if (respostaEscolhida == respostaCorrect) {
-        //console.log("Acertou")
         
         pontos += 10 // pontos = pontos + 10
     } else {
+       
         console.log("Errou!")
-        
+
     }
+    setTimeout(() => {
+        tirarPiscar()
+    }, 50);
 
     quantidadeDeQuestoes = parseInt(total.textContent)
     //console.log("Total " + quantidadeDeQuestoes)
@@ -124,25 +128,53 @@ function atualizarPlacar() {
 
 function fimDoJogo() {
 
-    
+
 
     let s = 's'
     pontos == 0 ? s = '' : s = s
-    instrucoes.textContent = "Fim de Jogo! Você conseguiu " + pontos + " ponto" + s
-    console.log(pontos, "pontos")
-    instrucoes.classList.add('placar')
+    const resmedia = (pontos / 12);
+    media.value = parseFloat(resmedia).toFixed(2)
+    if (resmedia >= 6) {
+
+        situacao.value = "Aprovado"
+        
+    } else if (resmedia <= 4) {
+
+        situacao.value = "Reprovado"
+    } else {
+
+        situacao.value = "Em-Recuperação"
+    }
+
+   
+    conteudo.style.display = 'none';
+    resultado.style.display = "flex"
 
     // OCULTAR O ARTICLE DA QUESTAO
-  
 
-    setTimeout(function () {
-        pontos = 0 // zerar placar
-        instrucoes.classList.remove('placar')
 
-        // REINICIAR O JOGO
-       
-        proximaQuestao(1)
-        instrucoes.textContent = 'Leia a questão e clique na resposta correta'
-    }, 7000)
 
+}
+botao.addEventListener("click", () => {
+    
+    pontos = 0 // zerar placar
+    instrucoes.classList.remove('placar')
+    conteudo.style.display = 'flex';
+    resultado.style.display = "none"
+    proximaQuestao(1)
+    
+})
+function piscarNoAcerto() {
+    articleQuestoes.classList.remove('errou')
+    articleQuestoes.classList.add('acertou')
+}
+
+function piscarNoErro() {
+    articleQuestoes.classList.remove('acertou')
+    articleQuestoes.classList.add('errou')
+}
+
+function tirarPiscar() {
+    articleQuestoes.classList.remove('acertou')
+    articleQuestoes.classList.remove('errou')
 }
